3e10a2d81bb00b0b661cb7342664dd801cfbaadf
Gonzalo Rodriguez <g_rod@live.cl>
2020-09-26 02:59:11 -0300

    Gonzalo - Adding linter.

--- src/service/__tests__/crawlerService.test.js
+++ src/service/__tests__/crawlerService.test.js
@@ -1,26 +1,25 @@
-"use strict";
+'use strict';
 const axios = require('axios');
 
 jest.mock('axios');
 
-const { getSingleProductDataFromUrl } = require("../crawlerService")
-const testData = require("./parisResponse")
+const {getSingleProductDataFromUrl} = require('../crawlerService');
+const testData = require('./parisResponse');
 
-describe("Should crawl single page", function() {
-  it("should get info from a url and get name and price", async function() {
-    const url = "http://foo.bar/product1";
+describe('Should crawl single page', function() {
+  it('should get info from a url and get name and price', async function() {
+    const url = 'http://foo.bar/product1';
     const crawlerPaths = {
-        pricePath: "string(//div[@class='item-price offer-price price-tc cencosud-price-2']/text())",
-        skuPath: "string(//p[@class='pull-right']/text())"
-    }
+      pricePath: 'string(//div[@class=\'item-price offer-price price-tc cencosud-price-2\']/text())',
+      skuPath: 'string(//p[@class=\'pull-right\']/text())',
+    };
 
     axios.get.mockImplementationOnce(() => Promise.resolve({data: testData}));
-    
-    const result =  await getSingleProductDataFromUrl(url, crawlerPaths);
+
+    const result = await getSingleProductDataFromUrl(url, crawlerPaths);
 
     expect(axios.get).toHaveBeenCalledWith('http://foo.bar/product1');
-    expect(result.price).toBe("179990");
-    expect(result.sku).toBe("505664999");
+    expect(result.price).toBe('179990');
+    expect(result.sku).toBe('505664999');
   });
-
 });
--- src/service/crawlerService.js
+++ src/service/crawlerService.js
@@ -1,46 +1,45 @@
 const axios = require('axios');
-const dom = require('xmldom').DOMParser
+const dom = require('xmldom').DOMParser;
 const xpath = require('xpath');
 
 // cron job
 
 // read db for competitor products and iterate through url
 const processSellerProducts = (sellerId) => {
-    // Get seller crawler paths
-    const crawlerPaths = {
-        pricePath: "string(//div[@class='item-price offer-price price-tc cencosud-price-2']/text())",
-        skuPath: "string(//p[@class='pull-right']/text())"
-    }
+  // Get seller crawler paths from db
+  const crawlerPaths = {
+    pricePath: 'string(//div[@class=\'item-price offer-price price-tc cencosud-price-2\']/text())',
+    skuPath: 'string(//p[@class=\'pull-right\']/text())',
+  };
 
-    // Get product list from repo by sellerId
-    const products = [
-        {
-            url: "https://www.paris.cl/set-bistro-mesa-hierro-2-sillas-outzen-505664999.html"
-        },
-        {
-            url: "https://www.paris.cl/set-bistro-mesa-hierro-2-sillas-outzen-505664999.html"
-        }
-    ]
-    // iterate
-    for (const product of products){
-        // call getSingleProductDataFromUrl with prod url
-        const crawledPrice = getSingleProductDataFromUrl(product.url, crawlerPaths);
-        // save price and sku on db
-    }
-}
+  // Get product list from repo by sellerId from db
+  const products = [
+    {
+      url: 'https://www.paris.cl/set-bistro-mesa-hierro-2-sillas-outzen-505664999.html',
+    },
+    {
+      url: 'https://www.paris.cl/set-bistro-mesa-hierro-2-sillas-outzen-505664999.html',
+    },
+  ];
+
+  // iterate
+  for (const product of products) {
+    // call getSingleProductDataFromUrl with prod url
+    const crawledPrice = getSingleProductDataFromUrl(product.url, crawlerPaths);
+    // save price and sku on db
+  }
+};
 
 // crawl by url on demand
 const getSingleProductDataFromUrl = async (url, crawlerPaths) => {
+  const product = {};
+  const {pricePath, skuPath} = crawlerPaths;
 
-    const product = {};
-    const { pricePath, skuPath} = crawlerPaths;
+  const response = await axios.get(url);
+  const doc = new dom().parseFromString(response.data, 'text/xml');
+  product.price = xpath.select(pricePath, doc).trim().split('$')[1].replace('.', '');
+  product.sku = xpath.select(skuPath, doc).replace('SKU ', '');
+  return product;
+};
 
-    const response = await axios.get(url);
-    const doc = new dom().parseFromString(response.data, "text/xml");
-    product.price = xpath.select(pricePath, doc).trim().split("$")[1].replace('.','');
-    product.sku = xpath.select(skuPath, doc).replace('SKU ', '');
-    return product;
-
-} 
-
-module.exports = { getSingleProductDataFromUrl }
+module.exports = {getSingleProductDataFromUrl};

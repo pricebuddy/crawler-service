6c6ff380ec4a4ba9fc96a2d4a27439521c224a48
Gonzalo Rodriguez <g_rod@live.cl>
2020-09-27 07:05:34 -0300

    Gonzalo - Fixing tests

--- src/service/__tests__/crawler-service.test.js
+++ src/service/__tests__/crawler-service.test.js
@@ -1,19 +1,16 @@
 const { crawlerService } = require('../crawler-service');
-const { getCompetitorSellers } = require('../../use-cases/get-sellers');
-const { processSellerProducts } = require('../../use-cases/process-seller-products');
-
-jest.mock('../../use-cases/get-sellers', () => ({
-  getCompetitorSellers: jest.fn().mockResolvedValue([{ id: 'foo' }, { id: 'foo' }]),
-}));
+const { getCompetitorSellers, getAllySellers } = require('../../use-cases/get-sellers');
+const { processAllyProducts, processCompetitorProducts } = require('../../use-cases/process-seller-products');
 
 jest.mock('../../use-cases/get-sellers', () => ({
   getAllySellers: jest.fn().mockResolvedValue([{ id: 'foo' }, { id: 'foo' }]),
+  getCompetitorSellers: jest.fn().mockResolvedValue([{ id: 'foo' }, { id: 'foo' }]),
 }));
 
 jest.mock('../../use-cases/process-seller-products', () => ({
-  processSellerProducts: jest.fn().mockResolvedValue({ id: 'foo' }),
+  processAllyProducts: jest.fn().mockResolvedValue({ id: 'foo' }),
+  processCompetitorProducts: jest.fn().mockResolvedValue({ id: 'foo' }),
 }));
-
 let fastify;
 
 describe('Should call service', () => {
@@ -21,9 +18,11 @@ describe('Should call service', () => {
     fastify = jest.fn();
   });
 
-  it('should call use case', async () => {
+  it('should call use case for allys', async () => {
     await crawlerService(fastify);
+    expect(getAllySellers).toHaveBeenCalled();
     expect(getCompetitorSellers).toHaveBeenCalled();
-    expect(processSellerProducts).toHaveBeenCalledTimes(2);
+    expect(processAllyProducts).toHaveBeenCalledTimes(2);
+    expect(processCompetitorProducts).toHaveBeenCalledTimes(2);
   });
 });
